297e8a483d97f4f757868ff029206380d36c1ced
Daniel Amorim Vilela de Salis <daniel.salis@agrotools.com.br>
2020-05-31 15:36:58 -0300

    .

---
+++ theGomesExtension/content.js
@@ -0,0 +1,23 @@
+console.log("Gomes extension runing!!!!!");
+
+let filenames = [
+    "foto1.png",
+    "foto2.png",
+    "foto3.png",
+    "foto4.png",
+    "foto5.png",
+    "foto6.png",
+    "foto7.png",
+    "foto8.png",
+    "foto9.png"
+];
+
+let imgs = document.getElementsByTagName('img');
+
+for(imgElement of imgs){
+    let r = Math.floor( Math.random()*filenames.length);
+    let file = 'Gomeses/'+filenames[r];
+    let url = chrome.extension.getURL(file);
+    imgElement.src = url;
+    console.log(url);
+}
